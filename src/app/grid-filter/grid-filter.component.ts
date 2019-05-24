47bbc94847d7a5766d9d1ab2697953e2e599e28e
mantuonweb <mantuonweb@hotmail.com>
2019-05-24 14:55:58 +0530

    2 files modified

--- src/app/grid-filter/grid-filter.component.ts
+++ src/app/grid-filter/grid-filter.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+//https://www.ag-grid.com/javascript-grid-filter-text/
 @Component({
   selector: 'app-grid-filter',
   templateUrl: './grid-filter.component.html',
   styleUrls: ['./grid-filter.component.scss']
 })
 export class GridFilterComponent implements OnInit {
-  filterTypes =['contains', 'notContains', 'equals', 'notEqual', 'startsWith', 'endsWith'];
+  filterTypes = [{
+    name:'contains',
+    displayName:'Contains',
+  },{
+    name:'notContains',
+    displayName:'notContains',
+  },{
+    name:'equals',
+    displayName:'equals',
+  },{
+    name:'notEqual',
+    displayName:'notEqual',
+  },{
+    name:'startsWith',
+    displayName:'startsWith',
+  },{
+    name:'endsWith',
+    displayName:'endsWith',
+  }];
   constructor() { }
 
   ngOnInit() {
